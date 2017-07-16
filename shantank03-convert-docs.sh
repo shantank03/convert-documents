0f68db22b7543b043b3fe714d23a59b8dfa02006
shantank <shantank@live.unc.edu>
2017-07-16 17:29:43 -0400

    created some documents

--- shantank03-convert-docs.sh
+++ shantank03-convert-docs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Declare input argument as a variable
-DOCUMENT=conversion-doc.md
+
 # Convert markdown to HTML
-pandoc -o conversion-doc.html $DOCUMENT
+pandoc -o conversion-doc.html shantank03-convert-documents/2017-07-16-conversion-document.md
 echo "The markdown document is being converted to HTML format."
 echo ""
 # Convert markdown to DOCX
-pandoc -o conversion-doc.docx $DOCUMENT
+pandoc -o conversion-doc.docx shantank03-convert-documents/2017-07-16-conversion-document.md
 echo "The markdown document is being converted to a MS Word document."
 echo ""
 # Convert markdown to ODT
-pandoc -o conversion-doc.odt $DOCUMENT
+pandoc -o conversion-doc.odt shantank03-convert-documents/2017-07-16-conversion-document.md
 echo"The markdown document is being converted to ODT format."
 echo ""
 # Convert markdown to PDF
-pandoc -o conversion-doc.pdf $DOCUMENT
+pandoc -o conversion-doc.pdf shantank03-convert-documents/2017-07-16-conversion-document.md
 echo "The markdown document is being converted to a PDF."
 echo ""
 # Convert markdown to TXT
-pandoc -o conversion-doc.txt $DOCUMENT
+pandoc -o conversion-doc.txt shantank03-convert-documents/2017-07-16-conversion-document.md
 echo "The markdown document is being converted to a TXT file."
 echo ""
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
